f1b8c5f5741a0bfad1c1323d5cafd9f117c31043
Luke Catton <luke.catton@hotmail.co.uk>
2026-02-10 15:00:23 +0000

    Adds deleted column to incident table

---
+++ database/migrations/2026_02_10_115625_add_incident_deleted_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('incidents', function ($table) {
+            $table->tinyInteger('deleted')->default(0)->after('updated_at');
+            $table->timestamp('deleted_at')->nullable()->after('deleted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('incidents', function ($table) {
+            $table->dropColumn('deleted');
+            $table->dropColumn('deleted_at');
+        });
+    }
+};
